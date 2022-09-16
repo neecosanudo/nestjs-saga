c002527df98541b984ada1925f4b0f656ffdbfaa
Nicolás Sañudo <nicolas.sanudo2@gmail.com>
2022-09-16 14:59:03 -0300

    🧑‍🎓 Class name: GET: cómo recibir parámetros

--- platzi-store/src/app.controller.ts
+++ platzi-store/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -13,5 +13,18 @@ export class AppController {
   @Get('nuevo')
   newEndpoint() {
     return 'yo soy nuevo';
+  }
+
+  @Get('products/:productId')
+  getProducts(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+
+  @Get('categories/:categoryId/products/:productId')
+  getCategory(
+    @Param('categoryId') categoryId: string,
+    @Param('productId') productId: string,
+  ) {
+    return `Product ${productId} and category ${categoryId}`;
   }
 }
