e95e42932d402a2e09e07daa140137fe45a4c6cf
Nicolás Sañudo <nicolas.sanudo2@gmail.com>
2022-09-19 15:56:57 -0300

    ✨ Add & update Users controller, service, entity and DTO.

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -6,42 +6,61 @@ import {
   Body,
   Put,
   Delete,
+  ParseIntPipe,
+  HttpStatus,
 } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from 'src/dtos/users.dto';
+import { UsersService } from 'src/services/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Get()
-  getAll() {
-    return {
-      message: 'Users list',
-    };
+  findAll() {
+    return this.usersService.findAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string) {
-    return {
-      message: `Users: ${id}`,
-    };
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+      }),
+    )
+    id: number,
+  ) {
+    return this.usersService.findOne(id);
   }
 
   @Post()
-  create(@Body() payload: any) {
-    return {
-      message: 'New users created',
-      payload: payload,
-    };
+  create(@Body() payload: CreateUserDto) {
+    return this.usersService.create(payload);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() payload: any) {
-    return {
-      id,
-      payload,
-    };
+  update(
+    @Param(
+      'id',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+      }),
+    )
+    id: number,
+    @Body() payload: UpdateUserDto,
+  ) {
+    return this.usersService.update(id, payload);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: number) {
-    return { id };
+  delete(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    return this.usersService.delete(id);
   }
 }
--- server/src/services/users.service.ts
+++ server/src/services/users.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from 'src/dtos/users.dto';
+import { User } from 'src/entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private counterId = 1;
+  private users: User[] = [
+    {
+      id: 1,
+      email: 'one@user.com',
+      password: 'user1password',
+    },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException(`User #${id} not found`);
+    }
+
+    return user;
+  }
+
+  create(payload: CreateUserDto) {
+    this.counterId += 1;
+    const newUser = {
+      id: this.counterId,
+      ...payload,
+    };
+
+    this.users.push(newUser);
+
+    return newUser;
+  }
+
+  update(id: number, payload: UpdateUserDto) {
+    const user = this.findOne(id);
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      throw new NotFoundException(`User #${id} not found`);
+    }
+
+    this.users[userIndex] = {
+      ...user,
+      ...payload,
+    };
+
+    return this.users[userIndex];
+  }
+
+  delete(id: number) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      throw new NotFoundException(`User #${id} not found`);
+    }
+
+    this.users.splice(userIndex, 1);
+
+    return true;
+  }
+}
